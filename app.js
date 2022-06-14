4980b5e467370e5ba3706a2b18e7813443bd2e00
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-06-14 16:38:14 +0530

    Added some conditional regarding NODE_ENV of production

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require('dotenv').config();
+}
 const pollRouter = require("./routes/poll");
 const router = require("./routes/testServer");
 const cors = require("cors");
